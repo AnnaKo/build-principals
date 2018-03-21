b85afc8b0ee21121de307acad448b106309d6064
Kanstantsin Klimov <kostya_klimov@list.ru>
2018-03-21 18:29:08 +0300

    Infinite loop added

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -16,7 +16,7 @@ class Project {
   public static void main(String[] args) {
     System.out.println(new Project().getGreeting());
   }
-  while (True){
+  while (True) {
     System.out.println("AAA-AAA-AAAA!");
   }
 }
