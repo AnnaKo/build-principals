ff3834e2eccce946593bd6234ec4a6f7c8d3fa1f
AlehYarmalovich <35954548+AlehYarmalovich@users.noreply.github.com>
2018-03-21 16:54:58 +0200

    Fix

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -16,15 +16,4 @@ class Project {
   public static void main(String[] args) {
     System.out.println(new Project().getGreeting());
   }
- 
-  private int x;
-  private int y;
-
-  public void setX(int val) { // Noncompliant: field 'x' is not updated
-    this.y = val;
-  }
-
-  public int getY() { // Noncompliant: field 'y' is not used in the return value
-    return this.x;
-  }
 }
