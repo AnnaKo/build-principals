caf2a7f72b4380ecbc0d079a2aa1c1247cc9f76b
AnnaKo <annakowork@gmail.com>
2018-03-21 18:36:07 +0200

    still trying

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -1,12 +1,13 @@
-
 package com.test;
+
+import org.apache.commons.collections.Predicate;
+import org.apache.log4j.Logger;
 
 class Project {
 
  public String getGreeting() {
         return "Hello, MNT Lab!";
     }
- /*infinity*/
 
   public static void test() {
     System.out.println("test");
@@ -14,9 +15,8 @@ class Project {
 
   public static void main(String[] args) {
     System.out.println(new Project().getGreeting());
-   int i;
-   for (i = 0; i < 10; i = i++) {
-    System.out.println(i);
-  }
+    while(true) {
+      System.out.println("AAA-AAA-AAAA!");
+    }
   }
 }
