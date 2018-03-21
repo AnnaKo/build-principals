36cd56374d41ee9096c855104150e21d9d528e20
Eugene Hodin <mushtarda@gmail.com>
2018-03-21 14:05:51 +0200

    Update Project.java

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -4,18 +4,18 @@ import org.apache.commons.collections.Predicate;
 import org.apache.log4j.Logger;
 
 class Project {
+ 
+ private static Logger logger = Logger.getLogger(Project.class.getName());
 
  public String getGreeting() {
         return "Hello, MNT Lab!";
     }
 
   public static void test() {
-    Logger logger = Logger.getLogger(LoggingJul.class.getName());
     logger.log("test");
   }
 
   public static void main(String[] args) {
-    Logger logger = Logger.getLogger(LoggingJul.class.getName());
     logger.log(new Project().getGreeting());
   }
 }
