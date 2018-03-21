7aad8b22e10e15c5aa6f17bf056ff5f733f8f12f
NikB97 <35953195+NikB97@users.noreply.github.com>
2018-03-21 18:23:32 +0200

    error 2

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -3,17 +3,15 @@ package com.test;
 import org.apache.commons.collections.Predicate;
 import org.apache.log4j.Logger;
 
-class Projdect {
-public static void teddddst() {
-    System.out.println("tddsdsest");
-  }
-
  public String getGreeting() {
         return "Hello, MNT Lab!";
     }
 
   public static void test() {
     System.out.println("test");
+      while(true) {
+       System.out.println("bag!");
+      }
   }
 
   public static void main(String[] args) {
