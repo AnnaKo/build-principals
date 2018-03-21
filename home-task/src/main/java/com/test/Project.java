c6ac001059886d5da089215884eb8a90318bf27d
Александр Матиев <c_m_@tut.by>
2018-03-21 15:56:12 +0200

    rollback

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -13,9 +13,6 @@ class Project {
 
   public static void main(String[] args) {
     System.out.println(new Project().getGreeting());
-   int i;
-   for (i = 0; i < 10; i = i++) {
-    System.out.println(i);
-  }
+  
   }
 }
