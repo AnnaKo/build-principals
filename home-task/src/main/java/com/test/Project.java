73ca81d3d1ab603af904fcef03351790f6010fe4
AlexandrSher <30312859+AlexandrSher@users.noreply.github.com>
2018-03-21 15:53:58 +0200

    Update Project.java

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -18,5 +18,13 @@ class Project {
   }
 }
 
-public int read(byte[] b)
-         throws IOException
+public void doSomethingWithFile(String fileName) {
+  BufferedReader buffReader = null;
+  try {
+    buffReader = new BufferedReader(new FileReader(fileName));
+    while (buffReader.readLine() != null) { // Noncompliant
+      // ...
+    }
+  } catch (IOException e) {
+    // ...
+  }
