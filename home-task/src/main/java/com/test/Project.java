1386c73cb17c522e1e4994029e1c5555b626a718
Eugene Hodin <mushtarda@gmail.com>
2018-03-21 14:23:53 +0200

    Role back

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -1,21 +1,19 @@
 package com.test;
 
-/*import org.apache.commons.collections.Predicate;*/
+import org.apache.commons.collections.Predicate;
 import org.apache.log4j.Logger;
 
 class Project {
- 
- private static Logger logger = Logger.getLogger(Project.class.getName());
 
  public String getGreeting() {
         return "Hello, MNT Lab!";
     }
 
   public static void test() {
-    logger.info("test");
+    System.out.println("test");
   }
 
   public static void main(String[] args) {
-    logger.info(new Project().getGreeting());
+    System.out.println(new Project().getGreeting());
   }
 }
