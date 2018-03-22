a14d8ab748a2c71d7e65327dbb353360dddfc850
pavelkislouski <36988338+pavelkislouski@users.noreply.github.com>
2018-03-22 15:55:37 +0200

    Update Project.java

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -7,25 +7,13 @@ class Project {
 
  public String getGreeting() {
         return "Hello, MNT Lab!";
-   
- }
+    }
 
   public static void test() {
     System.out.println("test");
   }
 
   public static void main(String[] args) {
-   int massive[];
-        massive = new int[10];
-        massive[10] = 10;
-   System.out.println(Integer.toString(massive[10]));
-   System.out.println("Hello Test");
-   int ab = 10;
-   System.out.println(Integer.toString(ab));
-   System.out.println(new Project().getGreeting());
-   while(1 == 1)
-   {
-    ab += 2;
-   }
-  }
+    System.out.println(new Project().getGreeting());
+}
 }
